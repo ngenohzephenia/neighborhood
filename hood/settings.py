be7e59c7c3b0a27871a8887c3ac5af76109b0da7
ngenohzephenia <ngenohzef@gmail.com>
2021-04-12 00:25:24 +0300

    added template and its files

--- hood/settings.py
+++ hood/settings.py
@@ -31,15 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap4',
-    'neighborhood',
-    'neighborhood.apps.NewsConfig',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
+    'neighborhood',
+   
 ]
 
 MIDDLEWARE = [
--- hood/urls.py
+++ hood/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     url(r'^neighborhood/',include('neighborhood.urls'))
+    path('', include ('neighborhood.urls') )
 ]
